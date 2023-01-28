67bb478e10613cb8f7dfb6cca24539e02f95257a
Martin Trajkovski <martintrajkovski70@gmail.com>
2023-01-29 00:22:04 +0100

    - Restructuring files, removed react client - Run configurations added for swagger

--- client/config-overrides.js
+++
@@ -1,8 +0,0 @@
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
-module.exports = function override(config, env) {
-  if (!config.plugins) {
-    config.plugins = [];
-  }
-  config.plugins.push(new MonacoWebpackPlugin({ languages: ['cpp', 'javascript', 'java'] }));
-  return config;
-};
